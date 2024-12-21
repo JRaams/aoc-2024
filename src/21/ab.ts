cbc2819d41d64eba5bcc91942baa7f393623b93a
JRaams <47593569+JRaams@users.noreply.github.com>
2024-12-21 19:46:52 +0100

    21 again

--- src/21/ab.ts
+++ src/21/ab.ts
@@ -15,84 +15,56 @@ const directionalMap = buildDirectionalMap();
 function getNumericSeqs(doorCode: string, map: KeypadMap): string[] {
   const result: string[] = [];
 
-  const queue: [string, string, string][] = [["A", doorCode, ""]];
+  const queue = [["A", doorCode, ""]];
 
   while (queue.length) {
-    const [current, str, path] = queue.shift()!;
-    if (str.length === 0) {
+    const [currentButton, remainingCode, path] = queue.shift()!;
+    if (remainingCode.length === 0) {
       result.push(path);
       continue;
     }
-    const char = str[0];
+    const newButton = remainingCode[0];
 
-    const options = map[current][char];
-    options.forEach((o) => {
-      queue.push([char, str.slice(1), path + o + "A"]);
+    const options = map[currentButton][newButton];
+    options.forEach((option) => {
+      queue.push([newButton, remainingCode.slice(1), path + option + "A"]);
     });
   }
 
   return result;
 }
 
-function shortestSeqLength(
-  start: string,
-  end: string,
-  depth: number,
-  cache: Map<string, number>
-): number {
-  const key = start + "_" + end + "_" + depth;
+const cache = new Map<string, number>();
+
+function shortestSeqLength(seq: string, depth: number) {
+  const key = `${seq}_${depth}`;
   if (cache.has(key)) return cache.get(key)!;
 
+  seq = "A" + seq;
+  let length = 0;
+
   if (depth === 1) {
-    return directionalMap[start][end][0].length;
+    for (let i = 0; i < seq.length - 1; i++) {
+      length += directionalMap[seq[i]][seq[i + 1]][0].length;
+    }
+    return length;
   }
 
-  let shortest = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < seq.length - 1; i++) {
+    let min = Number.MAX_SAFE_INTEGER;
 
-  Object.values(directionalMap[start][end])
-    .map((x) => "A" + x)
-    .forEach((path) => {
-      let length = 0;
-
-      for (let i = 0; i < path.length - 1; i++) {
-        length += shortestSeqLength(path[i], path[i + 1], depth - 1, cache);
-      }
-
-      shortest = Math.min(shortest, length);
+    directionalMap[seq[i]][seq[i + 1]].forEach((x) => {
+      const len = shortestSeqLength(x, depth - 1);
+      min = Math.min(min, len);
     });
 
-  cache.set(key, shortest);
-  return shortest;
+    length += min;
+  }
+
+  cache.set(key, length);
+  return length;
 }
 
-function shortestChainLength(
-  seqs: string[],
-  directionalRobots: number,
-  cache: Map<string, number>
-): number {
-  let shortest = Number.MAX_SAFE_INTEGER;
-
-  seqs
-    .map((x) => "A" + x)
-    .forEach((seq) => {
-      let length = 0;
-
-      for (let i = 0; i < seq.length - 1; i++) {
-        length += shortestSeqLength(
-          seq[i],
-          seq[i + 1],
-          directionalRobots,
-          cache
-        );
-      }
-
-      shortest = Math.min(shortest, length);
-    });
-
-  return shortest;
-}
-
-const cache = new Map<string, number>();
 let a = 0;
 let b = 0;
 
@@ -100,8 +72,8 @@ doorCodes.forEach((doorCode) => {
   const numericSeqs = getNumericSeqs(doorCode, numericalMap);
   const numeric = Number(doorCode.slice(0, -1));
 
-  a += numeric * shortestChainLength(numericSeqs, 2, cache);
-  b += numeric * shortestChainLength(numericSeqs, 25, cache);
+  a += numeric * Math.min(...numericSeqs.map((s) => shortestSeqLength(s, 2)));
+  b += numeric * Math.min(...numericSeqs.map((s) => shortestSeqLength(s, 25)));
 });
 
 console.log("a", a);
--- src/21/keypad.ts
+++ src/21/keypad.ts
@@ -9,69 +9,37 @@ const numericals: (string | undefined)[][] = [
   [undefined, "0", "A"],
 ];
 
-export function buildNumericalMap(): KeypadMap {
-  const numericalMap: KeypadMap = {};
-
-  for (let y = 0; y < numericals.length; y++) {
-    for (let x = 0; x < numericals[y].length; x++) {
-      const c = numericals[y][x];
-      if (c === undefined) continue;
-      numericalMap[c] = {};
-
-      const queue: [number, number, number, string][] = [[y, x, 0, ""]];
-      const seen = defaultDict(() => Number.MAX_SAFE_INTEGER);
-
-      while (queue.length) {
-        const [ny, nx, cost, path] = queue.shift()!;
-        if (numericals[ny]?.[nx] === undefined) continue;
-
-        const lowestCost = seen[`${ny}_${nx}`];
-        if (cost > lowestCost) continue;
-        seen[`${ny}_${nx}`] = cost;
-
-        const nc = numericals[ny][nx];
-        if (numericalMap[c][nc] === undefined) numericalMap[c][nc] = [];
-        numericalMap[c][nc].push(path);
-
-        queue.push([ny + 1, nx, cost + 1, path + "v"]);
-        queue.push([ny - 1, nx, cost + 1, path + "^"]);
-        queue.push([ny, nx + 1, cost + 1, path + ">"]);
-        queue.push([ny, nx - 1, cost + 1, path + "<"]);
-      }
-    }
-  }
-
-  return numericalMap;
-}
-
 const directions: (string | undefined)[][] = [
   [undefined, "^", "A"],
   ["<", "v", ">"],
 ];
 
-export function buildDirectionalMap(): KeypadMap {
-  const directionalMap: KeypadMap = {};
+function buildKeypadMap(
+  keys: (string | undefined)[][],
+  pathSuffix: string
+): KeypadMap {
+  const map: KeypadMap = {};
 
-  for (let y = 0; y < directions.length; y++) {
-    for (let x = 0; x < directions[y].length; x++) {
-      const c = directions[y][x];
+  for (let y = 0; y < keys.length; y++) {
+    for (let x = 0; x < keys[y].length; x++) {
+      const c = keys[y][x];
       if (c === undefined) continue;
-      directionalMap[c] = {};
+      map[c] = {};
 
       const queue: [number, number, number, string][] = [[y, x, 0, ""]];
       const seen = defaultDict(() => Number.MAX_SAFE_INTEGER);
 
       while (queue.length) {
         const [ny, nx, cost, path] = queue.shift()!;
-        if (directions[ny]?.[nx] === undefined) continue;
+        if (keys[ny]?.[nx] === undefined) continue;
 
         const lowestCost = seen[`${ny}_${nx}`];
         if (cost > lowestCost) continue;
         seen[`${ny}_${nx}`] = cost;
 
-        const nc = directions[ny][nx];
-        if (directionalMap[c][nc] === undefined) directionalMap[c][nc] = [];
-        directionalMap[c][nc].push(path + "A");
+        const nc = keys[ny][nx];
+        if (map[c][nc] === undefined) map[c][nc] = [];
+        map[c][nc].push(path + pathSuffix);
 
         queue.push([ny + 1, nx, cost + 1, path + "v"]);
         queue.push([ny - 1, nx, cost + 1, path + "^"]);
@@ -81,5 +49,13 @@ export function buildDirectionalMap(): KeypadMap {
     }
   }
 
-  return directionalMap;
+  return map;
+}
+
+export function buildNumericalMap() {
+  return buildKeypadMap(numericals, "");
+}
+
+export function buildDirectionalMap() {
+  return buildKeypadMap(directions, "A");
 }
